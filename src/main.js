1ea8c1c958835f2aabfc7b99cf24fdfe57ec4651
Cristian Navarro <canavarroq@ufpso.edu.co>
2023-06-21 11:11:33 -0500

    aux

--- src/main.js
+++ src/main.js
@@ -39,3 +39,73 @@ if (btnLogin) {
         }
     });
 }
+// Registration functionality
+const btnRegister = document.getElementById("btnRegister");
+if (btnRegister) {
+  btnRegister.addEventListener("click", () => {
+    // Retrieve the input values from the registration form
+    const inputFirstName = document.getElementById("inputFirstName").value;
+    const inputLastName = document.getElementById("inputLastName").value;
+    const inputEmail = document.getElementById("inputEmail").value;
+    const inputPassword = document.getElementById("inputPassword").value;
+    const inputAddress = document.getElementById("inputAddress").value;
+    const inputBirthday = document.getElementById("inputBirthday").value;
+    const alertErrorRegister = document.getElementById("alertErrorRegister");
+
+    if (
+      inputFirstName.length === 0 ||
+      inputLastName.length === 0 ||
+      inputEmail.length === 0 ||
+      inputPassword.length === 0 ||
+      inputConfirmPassword.length === 0 ||
+      inputAddress.length === 0 ||
+      inputBirthday.length === 0
+    ) {
+      alertErrorRegister.classList.remove("hidden");
+      alertErrorRegister.innerHTML = "<strong>Error!</strong> All fields are required";
+    } else if (inputPassword !== inputConfirmPassword) {
+      alertErrorRegister.classList.remove("hidden");
+      alertErrorRegister.innerHTML = "<strong>Error!</strong> Passwords do not match";
+    } else if (!passwordRegex.test(inputPassword)) {
+      alertErrorRegister.classList.remove("hidden");
+      alertErrorRegister.innerHTML =
+        "<strong>Error!</strong> Password must contain at least 8 characters, including uppercase and lowercase letters, and numbers";
+    } else {
+      alertErrorRegister.classList.add("hidden");
+
+      const data = {
+        firstName: inputFirstName,
+        lastName: inputLastName,
+        email: inputEmail,
+        password: inputPassword,
+        address: inputAddress,
+        birthday: inputBirthday,
+      };
+
+      const auth = new Auth();
+
+    // Call the register method and handle the response
+    auth.register(data)
+      .then(result => {
+        const statusCode = result.statusCode;
+        const responseData = result.data;
+
+        // Handle the response based on the status code
+        if (statusCode === 200) {
+          // Registration success
+          console.log("Registration successful", responseData);
+          location.href = "./login.html"; // Redireccionar a la página de inicio de sesión        } else {
+        } else {
+          // Registration failed
+          alertErrorRegister.classList.remove("hidden");
+          alertErrorRegister.innerHTML = `<strong>Error!</strong> ${responseData.data}`;
+        }
+      })
+      .catch(error => {
+        console.error("Error during registration", error);
+        alertErrorRegister.classList.remove("hidden");
+        alertErrorRegister.innerHTML = `<strong>Error!</strong> ${error}`;
+      });
+    }
+  });
+}
